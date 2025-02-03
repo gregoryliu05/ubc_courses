2944d2b51d3c8e2c02cd4310916052586ea35005
gliu25 <gregoryliu123@gmail.com>
2025-02-02 19:12:55 -0800

    added addDataset logic

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -1,4 +1,7 @@
 import { IInsightFacade, InsightDataset, InsightDatasetKind, InsightResult } from "./IInsightFacade";
+import JSZip, { JSZipObject } from "jszip";
+import { CourseInfo, Section } from "./insightTypes";
+require("jszip");
 
 /**
  * This is the main programmatic entry point for the project.
@@ -6,8 +9,58 @@ import { IInsightFacade, InsightDataset, InsightDatasetKind, InsightResult } fro
  *
  */
 export default class InsightFacade implements IInsightFacade {
+	// method for loading the dataset
+	// method for validating the data within the dataset and adding it to the json
+	// i need to get the current data
+	// then add all the current sections
+	private jszip = new JSZip();
+
+	private getValidCourses(data: JSZip): JSZipObject[] {
+		let courses: JSZipObject[] = [];
+		Object.entries(data.files).forEach(([name, object]) => {
+			if (name.includes("courses/") && !name.includes("__MACOSX") && name.length > 8) {
+				courses.push(object);
+			}
+		});
+		return courses;
+	}
+
 	public async addDataset(id: string, content: string, kind: InsightDatasetKind): Promise<string[]> {
-		// TODO: Remove this once you implement the methods!
+		// TODO: do validation, like to check if the id is valid or kind is valid or content is valid
+		// TODO: learn how to parse the data
+		// TODO: then write the tests
+		const data = await this.jszip.loadAsync(content, { base64: true });
+		let courses: JSZipObject[] = this.getValidCourses(data);
+		let sections: Section[] = [];
+
+		for (const course of courses) {
+			const info = await course.async("text");
+			JSON.parse(info).result.forEach((data: CourseInfo) => {
+				const section: Section = {
+					uuid: `${data.id}`,
+					id: id,
+					title: data.Title,
+					instructor: data.Professor,
+					dept: data.Subject,
+					year: data.Section === "overall" ? 1900 : parseInt(data.Year),
+					avg: data.Avg,
+					pass: data.Pass,
+					fail: data.Fail,
+					audit: data.Audit,
+				};
+				let check: Boolean = true;
+				for (const data in section) {
+					if (!data) {
+						check = false;
+					}
+				}
+				if (check) {
+					sections.push(section);
+				}
+			});
+		}
+		console.log(sections);
+
 		throw new Error(
 			`InsightFacadeImpl::addDataset() is unimplemented! - id=${id}; content=${content?.length}; kind=${kind}`
 		);
